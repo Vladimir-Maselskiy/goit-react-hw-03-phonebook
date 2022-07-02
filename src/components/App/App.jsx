9ee491f9001c90c44203ece4225b2e9c8d8cb9d1
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-07-02 21:11:46 +0300

    task 03.01

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,17 +4,30 @@ import { Contacts } from '../Contacts/Contacts';
 import Filter from '../Filter/Filter';
 import { Form } from '../Form/Form';
 import { Container, Title } from './App.styled';
+import defaultContacts from '../data/contacts';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
+      return;
+    }
+    this.setState({ contacts: defaultContacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    console.log('componentDidUpdate');
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleFormSubmit = data => {
     const { name, number } = data;
